cd56336e231f397076c5f7cf44dec048498c3647
hamiltonh <hamiltonhernandez@gmail.com>
2021-11-14 19:52:15 -0500

    set basic structure with next.js

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,7 @@ import 'tailwindcss/tailwind.css'
 import type { AppProps } from 'next/app'
 
 function MyApp({ Component, pageProps }: AppProps) {
+  console.log('Ejecutando el archivo pages/_app.tsx'); //Extrañamente ejecuta en la consola delbrowser y la terminal del server.!
   return <Component {...pageProps} />
 }
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,14 @@
 import type { NextPage } from 'next'
 import Link from 'next/link'
-// import Head from 'next/head'
+import Head from 'next/head'
 // import Image from 'next/image'
 // import styles from '../styles/Home.module.css'
 
 const Home: NextPage = () => {
   return (
    <div className="bg-green-500">
-    <div>Hola Mundo!</div>
+     <Head><title>Titulo de pagina | Gestion de Proyectos</title></Head>
+    <div><i className='fas fa-home'></i> Hola Mundo!</div>
     <Link href='admin/usuarios'>
       <a>Ruta prueba admin usuarios</a>
     </Link>
